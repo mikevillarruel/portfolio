fd7fd04bc1958e987a94f9193574b9a3b994c296
Michael Villarruel <alejovillarruel@gmail.com>
2022-12-21 00:09:50 -0500

    style: padding changed in about me page and page title added for each route

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,6 +5,7 @@ import { ErrorPage } from '../pages/Error';
 import { Home } from '../pages/Home';
 import { Projects } from '../pages/Projects';
 
+const name = 'Michael Villarruel'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -14,14 +15,17 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home />,
+        loader: () => (document.title = `${name} | Home`),
       },
       {
         path: 'about',
         element: <About />,
+        loader: () => (document.title = `${name} | About Me`),
       },
       {
         path: 'projects',
         element: <Projects />,
+        loader: () => (document.title = `${name} | Projects`),
       },
     ],
   },
